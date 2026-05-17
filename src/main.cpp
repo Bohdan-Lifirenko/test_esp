ef1c4b9283cd2ae87c0b3bb73e2a36fd254d661b
Lifirenko Bohdan <qp123456ex@gmail.com>
2026-05-17 17:31:03 +0300

    print_variables_millis

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include "Arduino.h"
 
+unsigned long lastPrint1 = 0;  // для змінної 1 (1 раз в секунду)
+unsigned long lastPrint2 = 0;  // для змінної 2 (2 рази в секунду)
+
+int variable1 = 10;
+int variable2 = 20;
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  
+  // Вивести змінну 1 (кожну 1000мс = 1 раз в секунду)
+  if (currentTime - lastPrint1 >= 1000) {
+    Serial.print("Variable 1: ");
+    Serial.println(variable1);
+    lastPrint1 = currentTime;
+  }
+  
+  // Вивести змінну 2 (кожну 500мс = 2 рази в секунду)
+  if (currentTime - lastPrint2 >= 500) {
+    Serial.print("Variable 2: ");
+    Serial.println(variable2);
+    lastPrint2 = currentTime;
+  }
 }
