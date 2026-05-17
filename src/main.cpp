b0f9bd976ace9944f9a51baefbcf9d709ce7e5a2
Lifirenko Bohdan <qp123456ex@gmail.com>
2026-05-17 17:15:39 +0300

    change esp32 board

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,7 @@
 void setup() {
   Serial.begin(115200);
   delay(1000);
-
-  Serial.println("Chip info:");
-
-  Serial.print("Chip model: ");
-  Serial.println(ESP.getChipModel());
-
-  Serial.print("Cores: ");
-  Serial.println(ESP.getChipCores());
-
-  Serial.print("Flash size: ");
-  Serial.println(ESP.getFlashChipSize());
 }
 
 void loop() {
-     Serial.print("Cores: ");
-     delay(1000);
 }
